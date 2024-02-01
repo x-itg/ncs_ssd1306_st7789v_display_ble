bc14f389adbed08394747562987b3420ec78c26a
gary <572981033@qq.com>
2024-02-01 10:54:07 +0800

    ncs display

--- ncs_st7789v_lvgl/build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/ncs/apps/lvgl;C:/ncs/v2.5.1.0/nrf;C:/ncs/v2.5.1.0/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "C:/ncs/apps/lvgl/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "C:/ncs/apps/lvgl/app.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
